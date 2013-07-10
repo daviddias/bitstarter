8454eac37030d954812222cbe27c397ea3653003
David Dias <daviddias.p@gmail.com>
2013-07-10 01:59:56 +0100

    ready for q1

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
-var express = require('express');
+var express = require('express')
+  , fs 		= require('fs')
 
-var app = express.createServer(express.logger());
+
+var app = express.createServer(express.logger())
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+  fs.readFile('./index.html' , function (err, content) {
+    response.send(content.toString())
+  })
+})
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
+})
